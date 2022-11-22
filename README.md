05c79d08b1b8c2b45b149d5057aee36f8b4ccc9c
Miso <28568777+FrancisYLfan@users.noreply.github.com>
2022-11-23 03:00:11 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # Rust Web 全栈开发教程【完结】（来自杨旭老师）.
 
+## 说明
 This is the code of video BV1RP4y1G7KF on bilibili. 
 
 这是B站 BV1RP4y1G7KF 这个视频的代码
+
+## Rust 版本, Rust verion
+
+```shell
+
+>> rustup --version
+
+rustup 1.25.1 (bb60b1e89 2022-07-12)
+info: This is the version for the rustup toolchain manager, not the rustc compiler.
+info: The currently active `rustc` version is `rustc 1.65.0 (897e37553 2022-11-02)`
+```
--- stage_1/http/src/httpresponse.rs
+++ stage_1/http/src/httpresponse.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::io::{Result, Write};
+
 #[derive(Debug, PartialEq, Clone)]
 pub struct HttpResponse<'a> {
     version: &'a str,
@@ -7,4 +8,51 @@ pub struct HttpResponse<'a> {
     status_text: &'a str,
     headers: Option<HashMap<&'a str, &'a str>>,
     body: Option<String>,
+}
+
+impl<'a> Default for HttpResponse<'a> {
+    fn default() -> Self {
+        Self {
+            version: "HTTP/1.1".into(),
+            status_code: "200".into(),
+            status_text: "OK".into(),
+            headers: None,
+            body: None,
+        }
+    }
+}
+
+impl<'a> HttpResponse<'a> {
+    pub fn new(
+        status_code: &'a str,
+        headers: Option<HashMap<&'a str, &'a str>>,
+        body: Option<String>,
+    ) -> HttpResponse<'a> {
+        let mut response: HttpResponse<'a> = HttpResponse::default();
+        if status_code != "200" {
+            response.status_code = status_code.into();
+        };
+        response.headers = match &headers {
+            Some(_h) => headers,
+            None => {
+                let mut h = HashMap::new();
+                h.insert("Content-Type", "text/html");
+                Some(h)
+            }
+        };
+
+        response.status_text = match response.status_code {
+            "200" => "OK".into(),
+            "400" => "Bad Request".into(),
+            "404" => "Not Found".into(),
+            "500" => "Internal Server Error".into(),
+            _ => "Not Found".into(),
+        };
+        response.body = body;
+        response
+    }
+
+    pub fn send_response(&self, write_stream:&mut impl Write)->Result<()>{
+        
+    }
 }
