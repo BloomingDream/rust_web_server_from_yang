067da32b16dd36dbb08f9a87275bd7b2343b0517
JakeWang <1875406398@qq.com>
2023-03-05 16:23:10 +0800

    不能正常获得页面，但是一直刷新，偶尔会得到正常页面问题。 问题是请求head过长，超过了200长度，修改为2083最长url长度

--- stage_1/httpserver/src/server.rs
+++ stage_1/httpserver/src/server.rs
@@ -19,7 +19,7 @@ impl<'a> Server<'a> {
         for stream in connection_listener.incoming() {
             let mut stream = stream.unwrap();
             println!("Connection established");
-            let mut read_buffer = [0; 200];
+            let mut read_buffer = [0; 1024];
             stream.read(&mut read_buffer).unwrap();
             let req: HttpRequest = String::from_utf8(read_buffer.to_vec()).unwrap().into();
 
